3856e78f90c5a50f762c6c63751a933a1130ab4b
beniusis <rokasbeniu@gmail.com>
2024-04-19 01:31:13 +0300

    feat: created a hook to retrieve the emojis

---
+++ src/hooks/useSearchEmoji.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function useSearchEmoji(value) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [foundAny, setFoundAny] = useState(false);
+  const [emojis, setEmojis] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    setFoundAny(false);
+
+    if (value === '') {
+      setLoading(false);
+      return;
+    }
+
+    const controller = new AbortController();
+
+    axios({
+      method: 'GET',
+      url: 'https://emoji-api.com/emojis',
+      params: { search: value, access_key: import.meta.env.VITE_EMOJI_API_KEY },
+      signal: controller.signal
+    })
+      .then((response) => {
+        setEmojis(response.data);
+        setFoundAny(response.data.length > 0);
+        setError(false);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+      });
+
+    return () => controller.abort();
+  }, [value]);
+
+  return { loading, error, foundAny, emojis };
+}
