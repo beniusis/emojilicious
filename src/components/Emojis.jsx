77f90ef9033f626aaac20f81f063dff9b0be0c0c
beniusis <rokasbeniu@gmail.com>
2024-04-20 21:26:40 +0300

    impr: roll back to informative emoji's title

--- src/components/Emojis.jsx
+++ src/components/Emojis.jsx
@@ -2,16 +2,10 @@ import Emoji from './Emoji';
 
 export default function Emojis({ emojis }) {
   return (
-    <div className="flex flex-row flex-wrap justify-center items-center gap-10 p-5">
+    <div className="flex flex-row flex-wrap items-center justify-center gap-10 p-5">
       {emojis.map((emoji) => {
         if (emoji.codePoint.length <= 5)
-          return (
-            <Emoji
-              key={emoji.codePoint}
-              value={emoji.character}
-              title={emoji.slug}
-            />
-          );
+          return <Emoji key={emoji.codePoint} value={emoji.character} />;
       })}
     </div>
   );
