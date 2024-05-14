ed4a2902b4a21167e3fe760c18e77d4b0ffb056e
beniusis <rokasbeniu@gmail.com>
2024-05-14 18:53:24 +0300

    chore: copy cursor on emoji hover

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -5,9 +5,9 @@ export default function Emoji({ value }) {
   };
 
   return (
-    <div className="flex h-[64px] w-[64px] select-none items-center justify-center rounded-lg text-3xl shadow-lg duration-100 ease-out hover:scale-110 hover:bg-gray-100 dark:bg-white dark:hover:bg-yellow-200">
+    <div className="flex h-[64px] w-[64px] select-none items-center justify-center rounded-lg text-3xl shadow-lg duration-100 ease-out hover:scale-110 hover:bg-gray-100 bg-white dark:hover:bg-yellow-200">
       <div
-        className="duration-100 ease-out max-h-fit max-w-fit hover:cursor-pointer active:scale-90"
+        className="duration-100 ease-out max-h-fit max-w-fit hover:cursor-copy active:scale-90"
         title="Press to copy to clipboard"
         onClick={copyToClipboard}
       >
