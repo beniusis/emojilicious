a75289159d0391ae45a5c2bc959072c129220084
beniusis <rokasbeniu@gmail.com>
2024-06-07 19:42:01 +0300

    impr: change the style of the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 export const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-center gap-8">
-      <h2 className="select-none text-5xl font-extrabold tracking-tight text-yellow-400 duration-500 ease-in-out sm:text-7xl">
+    <header className="relative -rotate-2 rounded-xl bg-emoji-yellow px-6 py-4 sm:px-8">
+      <h2 className="rotate-1 select-none text-4xl font-extrabold tracking-tight text-yellow-50 duration-500 ease-in-out sm:text-6xl">
         emojilicious
       </h2>
+
+      <span className="absolute -left-4 -top-2 text-4xl sm:-left-9 sm:-top-6 sm:text-6xl">🤪</span>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'emoji-yellow': 'rgb(255, 200, 61)'
+      }
+    }
   },
   plugins: []
 };
