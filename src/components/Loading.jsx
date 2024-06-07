4bd2651fae9c6eff7e9eb79e06125cb4e0cc84bd
beniusis <rokasbeniu@gmail.com>
2024-06-07 19:54:19 +0300

    impr: update Loading component with animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,3 +1,9 @@
 export const Loading = () => {
-  return <div className="text-3xl font-bold text-yellow-400">Loading...</div>;
+  return (
+    <div className="mt-8 flex flex-row gap-2">
+      <div className="size-4 animate-bounce rounded-full bg-emoji-yellow"></div>
+      <div className="size-4 animate-bounce rounded-full bg-emoji-yellow [animation-delay:-.3s]"></div>
+      <div className="size-4 animate-bounce rounded-full bg-emoji-yellow [animation-delay:-.5s]"></div>
+    </div>
+  );
 };
